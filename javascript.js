f888319a7241ee7e017a1eb767aa087bc73ad7e3
민권 <chlalsrnjs12@gmail.com>
2023-10-25 15:02:23 +0900

    비밀번호 숫자 4자리, 영화 리뷰 문자열 줄이기 추가

--- javascript.js
+++ javascript.js
@@ -44,7 +44,7 @@ const createCards = (item) => {
   newCard
     .querySelector(".rating")
     .append(`평점 ${item.vote_average} (${item.vote_count.toLocaleString()})`);
-  newCard.querySelector(".description").append(item.overview);
+  newCard.querySelector(".description").append(summaryOverview(item.overview));
   newCard.onclick = () => alert(`영화 id: ${item.id}`);
   mainContainer.append(newCard);
 };
@@ -54,5 +54,41 @@ function clickSearch() {
   fetchData(search);
 }
 document.querySelector("#search-button").onclick = clickSearch;
+
+// // 영화 설명 문자열 줄이기 (...)
+
+// const summaryOverview = (overview) => {
+//   let max = 300; // 표시할 글자수 기준
+//   if (overview.length > max) {
+//     overview =
+//       overview.substr(0, max - 2) +
+//       `
+//     더보기 ...`;
+//   }
+//   console.log(max);
+//   return overview;
+// };
+
+// // 비밀번호 숫자 4자리
+// function validatePassword() {
+//   let newPassword = document.getElementById("newPassword").value;
+//   let confirmPassword = document.getElementById("confirmPassword").value;
+//   let resultDiv = document.getElementById("result");
+
+//   let passwordRegex = /^[A-Za-z0-9](?:[~!@#$%^&*()_+=]+){6,12}$/;
+
+//   if (passwordRegex.test(newPassword)) {
+//     if (newPassword === confirmPassword) {
+//       resultDiv.innerHTML = "비밀번호가 확인되었습니다.";
+//       resultDiv.style.color = "green";
+//     } else {
+//       resultDiv.innerHTML = "비밀번호가 일치하지 않습니다.";
+//       resultDiv.style.color = "red";
+//     }
+//   } else {
+//     resultDiv.innerHTML = "올바른 비밀번호를 입력해 주시기 바랍니다.";
+//     resultDiv.style.color = "red";
+//   }
+// }
 
 fetchData();
